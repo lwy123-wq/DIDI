1056c9814d9b5cd8681901498e2963d489b2086a
lwy123-wq <l2158223895@qq.com>
2021-09-19 23:11:17 +0800

    didi

---
+++ src/test/java/com/sun/didi/DidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.didi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
