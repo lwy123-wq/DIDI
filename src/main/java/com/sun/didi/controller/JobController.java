1fc396b81aa5a0f7614e840700415e907d668447
lwy123-wq <l2158223895@qq.com>
2021-09-22 21:52:05 +0800

    didi

--- src/main/java/com/sun/didi/controller/JobController.java
+++ src/main/java/com/sun/didi/controller/JobController.java
@@ -1,4 +1,18 @@
 package com.sun.didi.controller;
 
+import com.sun.didi.service.JobServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class JobController {
+    @Autowired
+    private JobServiceImpl jobService;
+    @GetMapping(value = "/job")
+    @ResponseBody
+    public String job(String name,String phone,String id_code,String card,String school,String email,String marriage,String address,int years,String education){
+       return " " ;
+    }
 }
